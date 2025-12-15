181f94bbce485cad1f07805563334f13e075088d
anton <korolyov.anton0208@gmail.com>
2025-12-16 01:16:18 +0300

    .

--- Movies/app/src/main/java/com/example/movies/FavouriteMoviesActivity.java
+++ Movies/app/src/main/java/com/example/movies/FavouriteMoviesActivity.java
@@ -23,11 +23,11 @@ public class FavouriteMoviesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_favourite_movies);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         RecyclerView recyclerViewMovies = findViewById(R.id.recyclerViewMovies);
         MoviesAdapter moviesAdapter = new MoviesAdapter();
